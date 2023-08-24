e655cdb90e4e6a9b6910ae38e06c0b07ef034cd0
Artur Catunda <arturmcatunda@gmail.com>
2023-08-24 17:40:21 -0300

    feat: pivots change according to terrain

--- Player.gd
+++ Player.gd
@@ -1,42 +1,51 @@
 extends Node2D
 
+var width = 64
+var height = 64
+
+enum dir {RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3}
+
 var isShiftHeld = false
 
 var point
 var rotation_around_point = 0
-var rotationState = 2
+var rotationState = dir.DOWN
 
 var memes = 1
 var memeAux = 0
 
 var spriteBox
 var spriteSticky
-var Pivot1Sprite
-var Pivot2Sprite
-var Pivot3Sprite
-var Pivot4Sprite
+var Pivot1Sprite: Sprite2D
+var Pivot2Sprite: Sprite2D
+var Pivot3Sprite: Sprite2D
+var Pivot4Sprite: Sprite2D
+
+var currentTileMap: TileMap
 
 var ccwPivot
 var cwPivot
 
-func changeStickyDirection():
-	spriteSticky.rotation = (rotationState*PI/2)
-	
-	if(rotationState == 0):
+func changeStickyDirection():	
+	if(rotationState == dir.RIGHT):
 		ccwPivot = Pivot1Sprite
 		cwPivot = Pivot2Sprite
+		spriteSticky.rotation = (1*PI/2)
 	
-	elif(rotationState == 1):
+	elif(rotationState == dir.DOWN):
 		ccwPivot = Pivot2Sprite
-		cwPivot = Pivot4Sprite
-	
-	elif(rotationState == 2):
-		ccwPivot = Pivot4Sprite
 		cwPivot = Pivot3Sprite
+		spriteSticky.rotation = (2*PI/2)
 	
-	elif(rotationState == 3):
+	elif(rotationState == dir.LEFT):
 		ccwPivot = Pivot3Sprite
+		cwPivot = Pivot4Sprite
+		spriteSticky.rotation = (3*PI/2)
+	
+	elif(rotationState == dir.UP):
+		ccwPivot = Pivot4Sprite
 		cwPivot = Pivot1Sprite
+		spriteSticky.rotation = (0*PI/2)
 	
 
 func rotatePlayer(direction: String, angle):
@@ -54,7 +63,37 @@ func rotatePlayer(direction: String, angle):
 	
 
 func setPivots():
-	pass
+	var surroundingCells = currentTileMap.get_surrounding_cells(
+		currentTileMap.getMapPosition(global_position)
+	)
+	var surroundingWalls = surroundingCells.map(
+		func(cell): return currentTileMap.get_cell_source_id(0, cell)
+	)
+	var i = 0
+	
+	Pivot1Sprite.visible = false
+	Pivot2Sprite.visible = false
+	Pivot3Sprite.visible = false
+	Pivot4Sprite.visible = false
+	
+	surroundingWalls.reduce(
+		func(i, wall):
+			if(i == 0 && wall == 0):
+				Pivot1Sprite.visible = !Pivot1Sprite.visible
+				Pivot2Sprite.visible = !Pivot2Sprite.visible
+			if(i == 1 && wall == 0):
+				Pivot2Sprite.visible = !Pivot2Sprite.visible
+				Pivot3Sprite.visible = !Pivot3Sprite.visible
+			if(i == 2 && wall == 0):
+				Pivot3Sprite.visible = !Pivot3Sprite.visible
+				Pivot4Sprite.visible = !Pivot4Sprite.visible
+			if(i == 3 && wall == 0):
+				Pivot4Sprite.visible = !Pivot4Sprite.visible
+				Pivot1Sprite.visible = !Pivot1Sprite.visible
+			return i+1
+	, i)
+	
+	#print(surroundingWalls)
 
 #func meme(event):
 func _input(event):
@@ -69,39 +108,39 @@ func _input(event):
 	if(memeAux == 0):	
 		if event.is_action_pressed("ui_left"):
 			if(isShiftHeld):
-				rotationState = 3
+				rotationState = dir.LEFT
 				changeStickyDirection()
 			else:
-				if(rotationState == 2):
+				if(rotationState == dir.DOWN):
 					rotatePlayer("CW", (+cwPivot.position).angle())
-				elif(rotationState == 0):
+				elif(rotationState == dir.UP):
 					rotatePlayer("CCW", (-ccwPivot.position).angle())
 		elif event.is_action_pressed("ui_right"):
 			if(isShiftHeld):
-				rotationState = 1
+				rotationState = dir.RIGHT
 				changeStickyDirection()
 			else:
-				if(rotationState == 2):
+				if(rotationState == dir.DOWN):
 					rotatePlayer("CCW", (-ccwPivot.position).angle())
-				elif(rotationState == 0):
+				elif(rotationState == dir.UP):
 					rotatePlayer("CW", (+cwPivot.position).angle())
 		elif event.is_action_pressed("ui_up"):
 			if(isShiftHeld):
-				rotationState = 0
+				rotationState = dir.UP
 				changeStickyDirection()
 			else:
-				if(rotationState == 1):
+				if(rotationState == dir.RIGHT):
 					rotatePlayer("CCW", (+ccwPivot.position).angle())
-				elif(rotationState == 3):
+				elif(rotationState == dir.LEFT):
 					rotatePlayer("CW", (-cwPivot.position).angle())
 		elif event.is_action_pressed("ui_down"):
 			if(isShiftHeld):
-				rotationState = 2
+				rotationState = dir.DOWN
 				changeStickyDirection()
 			else:
-				if(rotationState == 1):
+				if(rotationState == dir.RIGHT):
 					rotatePlayer("CW", (-cwPivot.position).angle())
-				elif(rotationState == 3):
+				elif(rotationState == dir.LEFT):
 					rotatePlayer("CCW", (+ccwPivot.position).angle())
 	
 
@@ -115,9 +154,11 @@ func _ready():
 	Pivot3Sprite = get_node("Pivots/Pivot3")
 	Pivot4Sprite = get_node("Pivots/Pivot4")
 	
+	currentTileMap = get_node("../TileMap")
+	
 	global_position = Vector2i(64*9+32, 64*7+32)
 	
-	rotationState = 2
+	rotationState = dir.DOWN
 	changeStickyDirection()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -125,14 +166,15 @@ func _process(_delta):
 	
 	if(memeAux > 0):
 		rotation_around_point += (PI/2)/16 *memes
-		memeAux -= 1
 		
-#		if(rotation_around_point == 2*PI):
-#			rotation_around_point=0
+		memeAux -= 1
+		if(memeAux == 0):
+			setPivots()
+			
 		
 		global_position = point
 		global_position += Vector2(sin(rotation_around_point), 
-			cos(rotation_around_point)) * sqrt(32*32+32*32)
+			cos(rotation_around_point)) * sqrt(width/2*width/2+height/2*height/2)
 		spriteBox.rotation -= (PI/2)/16 *memes
 	
 
--- TileMap.gd
+++ TileMap.gd
@@ -10,15 +10,18 @@ const map = [[1,1,1,1,1,1,1,1,1,1,1,1],
 			 [1,0,0,0,0,0,0,0,0,0,0,1],
 			 [1,1,1,1,1,1,1,1,1,1,1,1]]
 
+func getMapPosition(pos):
+	return Vector2(floor(pos.x/tile_set.tile_size.x), floor(pos.y/tile_set.tile_size.y))
+
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():	
+func _ready():
 	for i in range(map.size()):
 		for j in range(map[0].size()):
 			set_cell(0, Vector2i(j,i), map[i][j]-1, Vector2i(0,0))
-			
-#	print(self.get_cell_source_id(0, Vector2i(1,1)))
+		
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
